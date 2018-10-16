941aa462d7b95f99178838546f930d9e79f868c5
Ryan Francis <rytfrancis@gmail.com>
2018-10-16 22:55:04 +0200

    added basic style

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
 (function() {
     let body = document.querySelector("body");
+    let inlay = document.querySelector(".textbox");
+    let button = document.querySelector(".button");
     if (!localStorage.getItem("choice")) {
         let choice = prompt("Do you prefer a light or dark site?");
         localStorage.setItem("choice", choice);
     }
-    let color = localStorage.getItem("choice");
-    console.log(color);
-    body.classList.add(color);
+    if (localStorage.getItem("choice") == "dark") {
+        body.classList.add("dark");
+        inlay.classList.add("darkInlay");
+        button.classList.add("darkButton");
+    } else if (localStorage.getItem("choice") == "light") {
+        body.classList.add("light");
+        inlay.classList.add("lightInlay");
+        button.classList.add("lightButton");
+    } else {
+        localStorage.removeItem("choice");
+    }
+    button.addEventListener("click", function() {
+        localStorage.removeItem("choice");
+        location.reload();
+    });
 })();
-
-// let choice = prompt("Do you prefer a light or dark site?");
-//
-// console.log(choice);
-// localStorage.setItem("choice", choice);
